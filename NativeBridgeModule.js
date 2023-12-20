cb6272d359abddc79f3f627a9be84be48dfc09a8
De2Housing <parjanya.shukla@housing.com>
2023-12-20 18:02:30 +0530

    Deleted old code

--- NativeBridgeModule.js
+++ NativeBridgeModule.js
@@ -2,25 +2,6 @@ import {NativeModules} from 'react-native';
 
 /**
  * @typedef {{
- *  action: 'navigateToHomeSearch' |
- *   'navigateToEdgeTab' |
- *   'navigateBack' |
- *   'navigateToPOC' |
- *   'premiumPurchaseDenied' |
- *   'premiumPurchasedSuccessful' |
- *   'navigateToPremium' |
- *   'navigateToHomeLoans' |
- *   'navigateToPdp' |
- *   'navigateToFavoriteProperties' |
- *   'navigateToContactedProperties' |
- *   'navigateToHelpAndSupport' |
- *   'navigateToCalculator';
- *  params: Record<string, any>;
- * }} NavigationAction
- */
-
-/**
- * @typedef {{
  *  isMediaPermissionGranted: (callback: Callback) => void;
  * }} NewBridgeModuleType
  * @returns {NewBridgeModuleType}
